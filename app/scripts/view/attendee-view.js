3f7e644e28a8f3ef9baf158eaa2ec737127a2570
Caleb Sattgast <calebsattgast@gmail.com>
2017-04-05 12:29:17 -0700

    build out basic templating functionality

--- app/scripts/view/attendee-view.js
+++ app/scripts/view/attendee-view.js
@@ -3,6 +3,26 @@
 (function(module) {
   var attendeeView = {};
 
+  var appendAttendee = function(attendeeData) {
+    // 'p' elements to be replaced with more approprate elements, using for now to demo js functionality
+    let attendeeTemplate = `<p class="attendee-name">${attendeeData.name}</p>`;
+
+    let scheduleTable = document.getElementById('schedule-table');
+    scheduleTable.insertAdjacentHTML('beforebegin', attendeeTemplate);
+
+    attendeeData.availability.forEach( (opt, index) => {
+      let optKey = `option${index + 1}`;
+      console.log('template', opt[optKey]);
+
+      // 'p' elements to be replaced with more approprate elements, using for now to demo js functionality
+      let availabilityTemplate = `<p class="attend-${opt[optKey]}">Can Attend? ${opt[optKey] ? 'Yes' : 'No'}</p>`;
+      console.log(availabilityTemplate);
+
+      let scheduleTable = document.getElementById('schedule-table');
+      scheduleTable.insertAdjacentHTML('beforebegin', availabilityTemplate);
+    });
+  };
+
   attendeeView.createAttendee = function() {
     console.log('Button was clicked');
     event.preventDefault();
@@ -13,13 +33,24 @@
     // Demo app is currently hard-coded for 5 options, but anticipating future capability to create polls with more or fewer options.
     for (var i = 2; i < event.target.length - 1; i++) {
       console.log(`Option ${i - 1}`, event.target[i], event.target[i].checked);
-      attendeeAvailability.push(event.target[i].checked);
+      attendeeAvailability.push({
+        [`option${i - 1}`] : event.target[i].checked
+      });
     };
 
     console.log(attendeeName);
     console.log(attendeeAvailability);
 
-    // var newAttendee = new Attendee(attendeeName, attendeeAvailability);
+    var newAttendee = new Attendee({
+      name: attendeeName,
+      availability: attendeeAvailability
+    });
+
+    Attendee.all.push(newAttendee);
+    console.log(newAttendee);
+    console.log(Attendee.all);
+
+    appendAttendee(newAttendee);
   };
 
   module.attendeeView = attendeeView;
