4e02786d4f3bf07719087ff287bcece2f84f2d83
Caleb Sattgast <calebsattgast@gmail.com>
2017-04-07 14:53:53 -0700

    dependencies, server setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 'use strict';
 
+// main dependencies
 const express = require('express');
+const attendeeRouter = require('./route/attendee-router.js');
 
+// dev tools
+const morgan = require('morgan');
+const debug = require('debug')('vennschedule:server');
+
+// app settings, server setup
 const PORT = 8080;
 const app = express();
+
+app.use(morgan('dev'));
+app.use(attendeeRouter);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
