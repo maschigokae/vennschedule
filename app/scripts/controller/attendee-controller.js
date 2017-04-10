4b90883714aee9ead868ca3090797c8c00d8928b
Caleb Sattgast <calebsattgast@gmail.com>
2017-04-09 19:17:09 -0700

    set up test ajax method, just logs data in console for now

--- app/scripts/controller/attendee-controller.js
+++ app/scripts/controller/attendee-controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function(module) {
+  Attendee.fetchAllAttendees();
 
   var attendeePoll = document.getElementById('schedule-selector');
   attendeePoll.addEventListener('submit', attendeeView.createAttendee);
--- app/scripts/model/attendee.js
+++ app/scripts/model/attendee.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const API_URL = 'http://localhost:8080';
+
 (function(module) {
+
   function Attendee(opts) {
     Object.keys(opts).forEach( (each, index, keys) => {
       this[each] = opts[each];
@@ -8,6 +11,27 @@
   };
 
   Attendee.all = [];
+
+  Attendee.fetchAllAttendees = function() {
+    // vanilla XMLHttpRequest goes here
+    var ajaxGetAllRequest = new XMLHttpRequest();
+    ajaxGetAllRequest.open('GET', `${API_URL}/api/attendee`, true);
+
+    ajaxGetAllRequest.onload = function(responseData) {
+      if (ajaxGetAllRequest.status === 200) {
+        var attendeeData = JSON.parse(ajaxGetAllRequest.responseText);
+        console.log(attendeeData);
+      } else {
+        console.log('Sorry, there was an error.');
+      }
+    };
+
+    ajaxGetAllRequest.onerror = function() {
+      // add error handling here
+    };
+
+    ajaxGetAllRequest.send();
+  };
 
   module.Attendee = Attendee;
 })(window);
