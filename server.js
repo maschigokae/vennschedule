81cb40c9264cefcc75acafd53dcfd82fcf4c1a71
Caleb Sattgast <calebsattgast@gmail.com>
2017-04-13 17:51:55 -0700

    remove hard-coded local port number

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 const debug = require('debug')('vennschedule:server');
 
 // app settings, server setup
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(morgan('dev'));
