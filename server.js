235e16b3d237f94c9ab6873dda98f8809fec1749
Caleb Sattgast <calebsattgast@gmail.com>
2017-04-13 19:12:47 -0700

    try using other cors middleware again

--- server.js
+++ server.js
@@ -2,22 +2,22 @@
 
 // main dependencies
 const express = require('express');
-// const cors = require('./backend-modules/lib/cors-middleware.js');
+const cors = require('./backend-modules/lib/cors-middleware.js');
 
 const attendeeRouter = require('./backend-modules/route/attendee-router.js');
 
 // dev tools
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const debug = require('debug')('vennschedule:server');
 
 // app settings, server setup
-const PORT = process.env.PORT || np;
+const PORT = process.env.PORT;
 const app = express();
 
-// app.use(cors());
-app.use(morgan('dev'));
+app.use(cors());
 // app.use(cors);
+app.use(morgan('dev'));
 app.use(attendeeRouter);
 app.use(express.static(`${__dirname}/webapp/public`));
 
